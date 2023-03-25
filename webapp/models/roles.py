c45f1cc0bf658d8de325f83a6da26715ab049626
wand3 <godswillbaiyewu@gmail.com>
2023-03-25 17:50:44 -0400

    roles model

--- webapp/models/roles.py
+++ webapp/models/roles.py
@@ -10,6 +10,14 @@ from sqlalchemy import Column, Integer, String, Text
 class Roles(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer(), primary_key=True)
-    role = db.Column(db.String(30), default='User')
-    
+    role = db.Column(db.String(30), unique=True)
+    role_desc = db.Column(db.String(100))
+
+    def __init__(self, role, role_desc):
+        self.role = role
+        self.role_desc= role_desc
+
+    def __repr__(self):
+        return "< Role {}: Description {}>".format(self.role, self.role_desc)
+        
 
